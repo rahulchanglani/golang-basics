cd317f236cb8b986f26875f16d36194594e88238
Rahul Changlani <rahul.changlani@gmail.com>
2021-12-27 11:18:36 +0530

    Added workflow yml

--- main.go
+++ main.go
@@ -6,5 +6,14 @@ var name = "Rahul"
 var num1 int
 
 func main() {
-	str3 := ""
+	str2 := "abc"
+	fmt.Println(num1)
+	fmt.Printf("%T", num1)
+	fmt.Printf("%v", name)
+	fmt.Printf("%q", str2)
+	var num2 = 123.45
+	fmt.Println(num2)
+	var str5 string
+	str5 = "xyz"
+	fmt.Printf("%s",str5)
 }
