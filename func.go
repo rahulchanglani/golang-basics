47eb9a4ef4ad7a0af296890d87a9c06bf52e5400
Rahul Changlani <rahul.changlani@gmail.com>
2021-12-27 14:50:45 +0530

    func#2

--- func.go
+++ func.go
@@ -4,8 +4,18 @@ import "fmt"
 
 func main() {
 	hello()
+	f1(6, 7)
+	add(88, 8)
 }
 
 func hello() {
 	fmt.Println("Hello")
+}
+
+func f1(a int, b int) {
+	fmt.Println(a + b)
+}
+
+func add(a, b int) int {
+	return a + b
 }
