7f9029fa0b7e28ad3615534535759177cf3ae25c
Rahul Changlani <rahul.changlani@gmail.com>
2021-12-27 13:05:55 +0530

    type conversion#6

--- typeConversion.go
+++ typeConversion.go
@@ -11,7 +11,7 @@ func main() {
 
 	var j string
 	// j = string(i)    // gives * on converting to string
-	j = strconv.Itoa(i)
+	j = strconv.Itoa(i) // I int to ascii
 	fmt.Printf("%v, %T\n", j, j)
 
 	var n1 int16 = 20
