a3646a58080d3bb2b82b03da9ae2137841a7166c
Rahul Changlani <rahul.changlani@gmail.com>
2021-12-27 12:00:01 +0530

    action#2

--- typematch.go
+++ typematch.go
@@ -5,12 +5,14 @@ import "fmt"
 const PI = 3.14
 const ID int = 89898
 
-func main() {
-	var num1 int16 = 20
-	var num2 int32 = 33
-	fmt.Println(num1 + int16(num2))
+var (
+	num1 int16 = 20
+	num2 int32 = 33
+)
 
-	var n1,n2,n3 int = 12,13,14
+func main() {
+	fmt.Println(num1 + int16(num2))
+	var n1, n2, n3 int = 12, 13, 14
 	fmt.Println(n1 + n2 + n3)
 
 	loc, sal := "BDC", 222
