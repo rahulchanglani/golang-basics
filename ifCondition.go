b8f050eed54e8700dc06a6138ee6a85e331a43b3
Rahul Changlani <rahul.changlani@gmail.com>
2021-12-27 12:27:04 +0530

    if condition#2

--- ifCondition.go
+++ ifCondition.go
@@ -11,4 +11,14 @@ func main() {
 	} else {
 		fmt.Println("else")
 	}
+
+	var n1,n2,n3 int
+	fmt.Scanln(&n1, &n2, &n3)
+		if n1>n2 && n1>n3 {
+			fmt.Println("n1 is largest")
+		} else if n2>n3 && n2>n1 {
+			fmt.Println("n2 is largest")
+		} else {
+			fmt.Println("n3 is largest")
+		}
 }
